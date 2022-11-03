9da595ea86b4971390cf76fdeb66b94f62390e99
John Barnes <ohenesetwumasi@gmail.com>
2022-11-03 19:55:57 +0000

    Feat: FOnt-fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <View className="px-5">
       <View>
-        <Text className="font-bold text-2xl font-bolder">Wallet</Text>
+        <Text className="text-2xl font-semi">Wallet</Text>
       </View>
       <View>
         <Text className="font-semibold text-gray-400">Active</Text>
